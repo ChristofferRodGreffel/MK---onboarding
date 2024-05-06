60ec8957fcf8151ac2410196817ef7bf80a6067e
Christoffer Rod Greffel <rodgaffel@gmail.com>
2024-05-06 07:58:34 +0200

    set content max width

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageWrapper = ({ children }) => {
   return (
-    <main className="content-grid mb-14 lg:w-4/5 lg:m-auto lg:mb-14">
+    <main className="content-grid mb-14 max-w-screen-xl lg:w-4/5 lg:m-auto lg:mb-14">
       {children}
     </main>
   );
