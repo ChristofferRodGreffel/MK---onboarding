e70010389fc874d78548927aae99519b3550883f
Christoffer Rod Greffel <rodgaffel@gmail.com>
2024-05-14 12:46:32 +0200

    upload firebaseConfig

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV17f6CWFjKygn7D56auNxId2AAjNOtYo",
+  authDomain: "maulund-kundeklub.firebaseapp.com",
+  projectId: "maulund-kundeklub",
+  storageBucket: "maulund-kundeklub.appspot.com",
+  messagingSenderId: "918874351877",
+  appId: "1:918874351877:web:2394e95f5912d9d3492f6c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
