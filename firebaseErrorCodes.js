8d047523a0ea3646e24df860e0bc019909ba6759
Christoffer Rod Greffel <rodgaffel@gmail.com>
2024-05-22 14:16:41 +0200

    Fejlbeskeder oversat til dansk

--- firebaseErrorCodes.js
+++ firebaseErrorCodes.js
@@ -1,16 +1,22 @@
 export const firebaseErrorsCodes = {
-  "auth/missing-password": "Please write a password on at least six (6) characters.",
-  "auth/user-not-found": "There is no user with this email.",
-  "auth/email-already-in-use": "There is already a user with this email.",
-  "auth/invalid-email": "Invalid email. \n\nPlease write a correct email.",
-  "auth/invalid-password": "Invalid password. \n\nThe password needs to be minimum six (6) characters.",
-  "auth/wrong-password": "Wrong password. \n\nIf you forgot, please reset it below.",
-  "auth/missing-email": "Please write an email.",
+  "auth/missing-password":
+    "Indtast venligst en adgangskode på mindst seks (6) tegn.",
+  "auth/user-not-found": "Der er ingen bruger med denne e-mail.",
+  "auth/email-already-in-use": "Der er allerede en bruger med denne e-mail.",
+  "auth/invalid-email":
+    "Ugyldig e-mail. \n\nIndtast venligst en korrekt e-mail.",
+  "auth/invalid-password":
+    "Ugyldig adgangskode. \n\nAdgangskoden skal være mindst seks (6) tegn.",
+  "auth/wrong-password":
+    "Forkert adgangskode. \n\nHvis du har glemt den, bedes du nulstille den nedenfor.",
+  "auth/missing-email": "Indtast venligst en e-mail.",
   "auth/too-many-requests":
-    "You have tried too many times with wrong informaiton. \nPlease try again in 30-60 seconds.\n\n If you type wrong information again, the waittime may be longer. \n\nIf you forgot your password, please reset it below.",
-  "auth/weak-password": "Invalid password. \n\nThe password needs to be minimum six (6) characters.",
-  "auth/internal-error": "Please check if you have filled all fields.",
-  "auth/user-disabled": "The account is disabled. Contact the company for more information",
-  "auth/invalid-login-credentials": "Wrong email or password.",
-  "auth/invalid-credential": "Wrong email or password.",
+    "Du har forsøgt for mange gange med forkerte oplysninger. \nPrøv igen om 30-60 sekunder.\n\nHvis du indtaster forkerte oplysninger igen, kan ventetiden blive længere. \n\nHvis du har glemt din adgangskode, bedes du nulstille den nedenfor.",
+  "auth/weak-password":
+    "Ugyldig adgangskode. \n\nAdgangskoden skal være mindst seks (6) tegn.",
+  "auth/internal-error": "Tjek venligst, om du har udfyldt alle felter.",
+  "auth/user-disabled":
+    "Kontoen er deaktiveret. Kontakt virksomheden for mere information.",
+  "auth/invalid-login-credentials": "Forkert e-mail eller adgangskode.",
+  "auth/invalid-credential": "Forkert e-mail eller adgangskode.",
 };
